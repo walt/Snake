fafd10d8f124ced4ac58bafa47706cb7914e9531
Walt Grayson <walt@grysn.com>
2022-05-08 13:13:12 -0700

    Initial commit.

---
+++ source/main.lua
@@ -0,0 +1,186 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local gfx <const> = playdate.graphics
+local snakeSegments = nil
+local cellSize = nil
+local gridXCount = nil
+local gridYCount = nil
+local foodPosition = nil
+local direction = nil
+
+function myGameSetUp()
+    cellSize = 20
+    gridXCount = playdate.display.getWidth() / cellSize
+    gridYCount = playdate.display.getHeight() / cellSize
+
+    restart()
+
+    playdate.timer.keyRepeatTimerWithDelay(
+        500,
+        500,
+        myTimerClosure
+    )
+end
+
+function restart()
+    snakeSegments = {
+        {x = 3, y = 1},
+        {x = 2, y = 1},
+        {x = 1, y = 1},
+    }
+    directionQueue = {'right'}
+    moveFood()
+end
+
+function moveFood()
+    local possibleFoodPositions = {}
+    
+    for foodX = 1, gridXCount do
+        for foodY = 1, gridYCount do
+            local possible = true
+            
+            for segmentIndex, segment in ipairs(snakeSegments) do
+                if foodX == segment.x and foodY == segment.y then
+                    possible = false
+                end
+            end
+            
+            if possible then
+                table.insert(possibleFoodPositions, {x = foodX, y = foodY})
+            end
+        end
+    end
+    
+    foodPosition = possibleFoodPositions[
+        math.random(#possibleFoodPositions)
+    ]
+end
+
+function myTimerClosure()
+    if #directionQueue > 1 then
+        table.remove(directionQueue, 1)
+    end
+
+    -- set the next position based on
+    -- the current position and the direction
+
+    local nextXPosition = snakeSegments[1].x
+    local nextYPosition = snakeSegments[1].y
+
+    if directionQueue[1] == 'right' then
+        nextXPosition = nextXPosition + 1
+        if nextXPosition > gridXCount then
+            beepAndRestart()
+        end
+    elseif directionQueue[1] == 'left' then
+        nextXPosition = nextXPosition - 1
+        if nextXPosition < 1 then
+            beepAndRestart()
+        end
+    elseif directionQueue[1] == 'down' then
+        nextYPosition = nextYPosition + 1
+        if nextYPosition > gridYCount then
+            beepAndRestart()
+        end
+    elseif directionQueue[1] == 'up' then
+        nextYPosition = nextYPosition - 1
+        if nextYPosition < 1 then
+            beepAndRestart()
+        end
+    end
+
+
+    -- update the snake data
+
+    local canMove = true
+    
+    for segmentIndex, segment in ipairs(snakeSegments) do
+        if segmentIndex ~= #snakeSegments
+        and nextXPosition == segment.x
+        and nextYPosition == segment.y then
+            canMove = false
+        end
+    end
+
+    if canMove then
+        table.insert(snakeSegments, 1, {
+            x = nextXPosition, y = nextYPosition
+        })
+        
+        if snakeSegments[1].x == foodPosition.x and snakeSegments[1].y == foodPosition.y then
+            local s = playdate.sound.synth.new(playdate.sound.kWaveSine)
+            s:playMIDINote('A4', 0.5, 0.5)
+            -- s:playMIDINote('A5', 0.5, 0.5, 0.5)
+
+            moveFood()
+        else
+            table.remove(snakeSegments)
+        end
+    else
+        beepAndRestart()
+    end
+
+
+    -- clear the screen
+
+    gfx.clear()
+
+
+    -- redraw the food
+
+    gfx.drawRect(
+        ((foodPosition.x - 1) * cellSize) + 2,
+        ((foodPosition.y - 1) * cellSize) + 2,
+        cellSize - 4,
+        cellSize - 4
+    )
+
+
+    -- redraw the snake
+
+    for segmentIndex, segment in ipairs(snakeSegments) do
+        gfx.fillRect(
+            (segment.x - 1) * cellSize,
+            (segment.y - 1) * cellSize,
+            cellSize,
+            cellSize
+        )
+    end
+end
+
+function beepAndRestart()
+    local s = playdate.sound.synth.new(playdate.sound.kWaveSine)
+    s:playMIDINote('C5', 0.5, 0.5)
+    -- s:playMIDINote('C4', 0.5, 0.5, 0.5)
+    restart()
+end
+
+myGameSetUp()
+
+function playdate.update()
+    if playdate.buttonIsPressed( playdate.kButtonRight )
+    and directionQueue[#directionQueue] ~= 'right'
+    and directionQueue[#directionQueue] ~= 'left' then
+        table.insert(directionQueue, 'right')
+    end
+    if playdate.buttonIsPressed( playdate.kButtonLeft )
+    and directionQueue[#directionQueue] ~= 'left'
+    and directionQueue[#directionQueue] ~= 'right' then
+        table.insert(directionQueue, 'left')
+    end
+    if playdate.buttonIsPressed( playdate.kButtonUp )
+    and directionQueue[#directionQueue] ~= 'up'
+    and directionQueue[#directionQueue] ~= 'down' then
+        table.insert(directionQueue, 'up')
+    end
+    if playdate.buttonIsPressed( playdate.kButtonDown )
+    and directionQueue[#directionQueue] ~= 'down'
+    and directionQueue[#directionQueue] ~= 'up' then
+        table.insert(directionQueue, 'down')
+    end
+
+    playdate.timer.updateTimers()
+end
